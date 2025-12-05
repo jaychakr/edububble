e531ce17a4b864078b075b5e6103085e2f85f5e4
Jay Chakravarty <jaychakravarty@icloud.com>
2025-12-05 14:33:03 -0800

    update .webp to .jpg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import constructionWorker from './assets/construction-worker.svg'
-import edububble from './assets/edububble.webp'
+import edububble from './assets/edububble.jpg'
 import Header from "./components/Header.tsx"
 import Footer from "./components/Footer.tsx"
 import './App.css'
@@ -8,14 +8,13 @@ function App() {
   return (
     <>
       <Header />
-      <img className="patience" src={edububble}/>
+      <img className="edububble" src={edububble}/>
       <div className="card">
         <img src={constructionWorker} className="logo" alt="Construction worker" />
         <p>
-          <b>edububble.com</b> is currently under construction
+          This site is currently under construction and will become available for new users at <a href="https://edububble.com"><b>edububble.com</b></a> once development is complete.
         </p>
       </div>
-      
       <Footer />
     </>
   )
